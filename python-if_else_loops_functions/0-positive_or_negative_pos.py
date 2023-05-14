9ac2abd6d52cd12904b4d5cba86c9cfe8bcbe041
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-05-14 03:20:21 -0400

    Update 0-positive_or_negative_pos.py

--- python-if_else_loops_functions/0-positive_or_negative_pos.py
+++ python-if_else_loops_functions/0-positive_or_negative_pos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
 # YOUR CODE HERE
 # Print the number and its sign
 print(number, end=' ')
