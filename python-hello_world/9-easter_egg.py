1f77db8a2b2385e13a55bfc8442a988bd7137eed
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-05-22 12:02:03 -0400

    Update 9-easter_egg.py

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python3
-print("The Zen of Python, by Tim Peters")
-print("\nBeautiful is better than ugly.")
-print("Explicit is better than implicit.")
-print("Simple is better than complex.")
-print("Complex is better than complicated.")
-print("Flat is better than nested.")
-print("Sparse is better than dense.")
-print("Readability counts.")
-print("Special cases aren't special enough to break the rules.")
-print("Although practicality beats purity.")
-print("Errors should never pass silently.")
-print("Unless explicitly silenced.")
-print("In the face of ambiguity, refuse the temptation to guess.")
-print("There should be one-- and preferably only one --obvious way to do it.")
-print("Although that way may not be obvious at first unless you're Dutch.")
-print("Now is better than never.")
-print("Although never is often better than *right* now.")
-print("If the implementation is hard to explain, it's a bad idea.")
-print("If the implementation is easy to explain, it may be a good idea.")
-print("Namespaces are one honking great idea -- let's do more of those!")
+print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nPracticality beats purity.\nErrors should never pass silently.\nUnless silenced.\nIn the face of ambiguity, refuse to guess.\nOne obvious way to do it.\nAlthough not always obvious.\nNow is better than never.\nAlthough never is often better than now.\nIf hard to explain, it's a bad idea.\nIf easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more!")
+
 
 
