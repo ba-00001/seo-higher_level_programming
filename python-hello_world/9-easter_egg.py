5ff7da3575e66ea3de67a315961977c7fd2a829e
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-05-22 01:06:42 -0400

    Update 9-easter_egg.py

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nPracticality beats purity.\nErrors should never pass silently.\nUnless silenced.\nIn the face of ambiguity, refuse to guess.\nOne obvious way to do it.\nAlthough not always obvious.\nNow is better than never.\nAlthough never is often better than now.\nIf hard to explain, it's a bad idea.\nIf easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more!")
+print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.")
 
