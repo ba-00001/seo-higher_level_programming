df83da6e7716765c965d77b563525aad10f7a5cf
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-05-22 01:08:45 -0400

    Update 9-easter_egg.py

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.")
+str = "is"
+print(f"The Zen of Python, by Tim Peters\n\nBeautiful {str}{str} better than ugly.")
 
