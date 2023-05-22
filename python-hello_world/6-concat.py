a72e3cc3036a6699313732d697edbd2fc7de1586
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-05-21 23:56:41 -0400

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = " Holberton School!"
+str1 = "to"
+str2 = "Holberton School"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"{str1 + str2}")
+print(f"Welcome {str1} {str2}!")
