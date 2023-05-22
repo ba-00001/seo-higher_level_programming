f38b63ed71f576daaf3d3c47c5a41e05755c9292
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-05-22 00:23:18 -0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[40:63])
-
+print(str[39:67])
