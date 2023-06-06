6ad1df2669823a410ff19878dfeefacb78252245
ba-00001 <ba.00001@outlook.com>
2023-06-05 22:29:46 -0400

    update

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,14 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 prefix = "Last digit of"
 suffix = "is"
+
+
 if number <= 0:
     last_digit = -1 * last_digit
+str1 = f"{prefix} {number} {suffix} {last_digit}"
 if last_digit > 5 and number >= 0:
-    print(f"{prefix} {number} {suffix} {last_digit} and is greater than 5")
+    print(str1 + " and is greater than 5")
 elif last_digit == 0:
-    print(f"{prefix} {number} {suffix} {last_digit} and is 0")
+    print(str1 + " and is 0")
 else:
-    print(f"{prefix} {number} {suffix} {last_digit} and is less than 6 and not 0")
+    print(str1 + " and is less than 6 and not 0")
