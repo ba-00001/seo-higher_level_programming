7473e21c5cae9614fe443488c02cb30f6fe42d50
ba-00001 <ba.00001@outlook.com>
2023-06-05 21:20:39 -0400

    update

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ prefix = "Last digit of"
 suffix = "and is"
 
 if last_digit > 5:
-    print(f"{prefix} {number} {suffix} greater than 5")
+    print(f"{prefix} {number} {suffix} greater than 5 \n")
 elif last_digit == 0:
-    print(f"{prefix} {number} {suffix} 0")
+    print(f"{prefix} {number} {suffix} 0\n")
 else:
-    print(f"{prefix} {number} {suffix} less than 6 and not 0")
+    print(f"{prefix} {number} {suffix} less than 6 and not 0\n")
