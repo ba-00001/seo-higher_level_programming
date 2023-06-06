25febefc01e130a052dabc08720e194d018089d1
ba-00001 <ba.00001@outlook.com>
2023-06-05 20:45:58 -0400

    update

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+prefix = "Last digit of"
+suffix = "and is"
+
+if last_digit > 5:
+    print(f"{prefix} {number} {suffix} greater than 5")
+elif last_digit == 0:
+    print(f"{prefix} {number} {suffix} 0")
+else:
+    print(f"{prefix} {number} {suffix} less than 6 and not 0")
