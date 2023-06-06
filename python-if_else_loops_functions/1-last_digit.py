7e4d81909d2907f895c80a6773dfe7329b8cd60c
ba-00001 <ba.00001@outlook.com>
2023-06-05 22:02:28 -0400

    update

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,8 +12,10 @@ else:
     sign = "-"
 
 if last_digit > 5 and number >= 0:
-    print(f"{prefix} {number} {suffix} {sign}{last_digit} and is greater than 5")
+    output = f"{prefix} {number} {suffix} {sign}{last_digit} and is greater than 5"
 elif last_digit == 0:
-    print(f"{prefix} {number} {suffix} {sign}{last_digit} and is 0")
+    output = f"{prefix} {number} {suffix} {sign}{last_digit} and is 0"
 else:
-    print(f"{prefix} {number} {suffix} {sign}{last_digit} and is less than 6 and not 0")
+    output = f"{prefix} {number} {suffix} {sign}{last_digit} and is less than 6 and not 0"
+
+print(output)
