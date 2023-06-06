2ca3baf9cd92ad50593c78d6881bbaf44c6b456b
ba-00001 <ba.00001@outlook.com>
2023-06-05 21:40:29 -0400

    update

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-    print(str(number) + " is positive")
+    print(str(number) + " is positive\n")
 elif number == 0:
-    print(str(number) + " is zero")
+    print(str(number) + " is zero\n")
 else:
-    print(str(number) + " is negative")
+    print(str(number) + " is negative\n")
