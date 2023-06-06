4c483f853b5e9902a99335c71bfd05a82ddf9ca0
ba-00001 <ba.00001@outlook.com>
2023-06-05 21:54:32 -0400

    update

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,14 @@ last_digit = abs(number) % 10
 prefix = "Last digit of"
 suffix = "is"
 
-if last_digit > 5:
-    print(f"{prefix} {number} {suffix} {last_digit} and is greater than 5\n")
-elif last_digit == 0:
-    print(f"{prefix} {number} {suffix} {last_digit} and is 0\n")
+if number >= 0:
+    sign = ""
 else:
-    print(f"{prefix} {number} {suffix} {last_digit} and is less than 6 and not 0\n")
+    sign = "-"
+
+if last_digit > 5:
+    print(f"{prefix} {number} {suffix} {sign}{last_digit} and is greater than 5\n")
+elif last_digit == 0:
+    print(f"{prefix} {number} {suffix} {sign}{last_digit} and is 0\n")
+else:
+    print(f"{prefix} {number} {suffix} {sign}{last_digit} and is less than 6 and not 0\n")
