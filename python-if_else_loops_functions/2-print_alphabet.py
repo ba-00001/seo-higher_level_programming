62b046aefd1d26dcf5d8a0e223936c612ec88d50
ba-00001 <ba.00001@outlook.com>
2023-06-07 19:55:47 -0400

    update

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -3,9 +3,4 @@ for char_code in range(ord('a'), ord('z') + 1):
     print("{}".format(chr(char_code)), end='')
 
 
-# for char_code in range(ord('a'), ord('z') + 1):
-#     variable = chr(char_code)
-#     print(f'{variable}'.format(variable), end='')
-
-#print(f’{variable}’.format(variable)
 
