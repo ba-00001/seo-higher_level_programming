3de535b8108be8b90c72746b9101425ed202b016
ba-00001 <ba.00001@outlook.com>
2023-06-07 19:54:28 -0400

    update

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 for char_code in range(ord('a'), ord('z') + 1):
-    variable = chr(char_code)
-    print(f'{variable}'.format(variable), end='')
+    print("{}".format(chr(char_code)), end='')
+
+
+# for char_code in range(ord('a'), ord('z') + 1):
+#     variable = chr(char_code)
+#     print(f'{variable}'.format(variable), end='')
 
 #print(f’{variable}’.format(variable)
 
