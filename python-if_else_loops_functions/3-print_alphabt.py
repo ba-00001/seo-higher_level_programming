ab69519bb2006add0d29d2f1a0d716c48862bf1b
ba-00001 <ba.00001@outlook.com>
2023-06-07 20:45:21 -0400

    update

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 for char_code in range(ord(a), ord(z) + 1):
     if chr(char_code) != q and chr(char_code) != e:
         print("{}".format(chr(char_code)), end=)
