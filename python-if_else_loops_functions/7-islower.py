6bffb5b5b56df1e8b46a313c49f546f503abb6ef
ba-00001 <ba.00001@outlook.com>
2023-06-10 15:30:58 -0400

    update

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -4,29 +4,3 @@ def islower(c):
         return True
     else:
         return False
-
-
-
-"""
-           7. islower
-        mandatory
-        Write a function that checks for lowercase character.
-        
-        Prototype: def islower(c):
-        Returns True if c is lowercase
-        Returns False otherwise
-        You are not allowed to import any module
-        You are not allowed to use str.upper() and str.isupper()
-        Tips: ord()
-        You don’t need to understand __import__
-
-
-"""
-
-"""
-        The ord() function is used to get the ASCII value of the character c.
-        Lowercase letters in ASCII range from 97 to 122.
-        We compare the ASCII value of c with this range to check if it is lowercase.
-        If the condition is satisfied, we return True; otherwise, we return False.
-
-"""
