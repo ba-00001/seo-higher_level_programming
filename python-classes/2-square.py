01aa7e983975e33437cca1c90f0f5e91a5a875ba
ba-00001 <ba.00001@outlook.com>
2023-06-17 23:57:28 -0400

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
-""" Module is documented"""
+"""
+Module: square
+This module contains the Square class.
+"""
 
 
 class Square:
-    """ Module is documented"""
-    def __init__(self, size=0):
+    """
+    Square class represents a square shape.
 
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square (default 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
