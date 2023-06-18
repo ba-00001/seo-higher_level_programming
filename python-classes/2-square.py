2e9b30b3ac84f14df405f6e69ad3c713db21250a
ba-00001 <ba.00001@outlook.com>
2023-06-17 23:55:47 -0400

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,6 +4,7 @@
 
 class Square:
     def __init__(self, size=0):
+        """ Module is documented"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
