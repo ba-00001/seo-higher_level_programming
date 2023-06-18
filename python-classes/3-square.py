deda01b3c5b302e1399971a0eb659225eee773ef
ba-00001 <ba.00001@outlook.com>
2023-06-18 00:04:42 -0400

    update

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""
+Module: square
+This module contains the Square class.
+"""
+
+
+class Square:
+    """
+    Square class represents a square shape.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square (default 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
