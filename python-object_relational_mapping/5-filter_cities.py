0da6ab284d013af1a3279aaa66d762d80071e095
ba-00001 <ba.00001@outlook.com>
2023-06-19 03:22:17 -0400

    update

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script that lists all cities of a
+ given state from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query to retrieve cities of the given state
+    cursor.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities \
+                    JOIN states ON cities.state_id = states.id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id ASC", (state_name,))
+
+    # Fetch the first row
+    row = cursor.fetchone()
+
+    # Display the results
+    if row:
+        print(row[0])
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
