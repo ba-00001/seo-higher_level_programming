bc7baf7789d676f4a8e38030b60f928f0f9f3bbe
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-28 19:10:22 -0400

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-""" This module is used to link a class to a table in a database. """
-
+"""Start link class to table in database"""
 
 import sys
-
-from model_state import Base, State
-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/\
-    {}'.format(sys.argv[1], sys.argv[2],
-    sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+# Create an instance of Base as the base class for class definitions
+Base = declarative_base()
 
 
 class State(Base):
-    """
-    Represents a state in the database with attributes id and name.
+    """State class representing a state entity in the MySQL table 'states'."""
 
-    Attributes:
-        id (int): An auto-generated, unique integer representing the state's ID.
-        name (str): A string representing the state's name.
-    """
-    pass
+    # Table name in the database
+    __tablename__ = 'states'
+
+    # Columns
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        return f"<State(id={self.id}, name='{self.name}')>"
+
+
+if __name__ == "__main__":
+    # Connect to the MySQL server using command-line arguments
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the table(s) based on the class definitions
+    Base.metadata.create_all(engine)
