80497c7c6e7617a486cd6070f7ba07501aecc32a
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-28 19:11:00 -0400

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Start link class to table in database"""
 
+
 import sys
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
