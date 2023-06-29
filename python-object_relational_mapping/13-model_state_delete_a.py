55023208c9dcbdc623f21a8264e596587962f542
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-06-28 20:03:13 -0400

    update

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,7 +10,9 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     # Connect to the MySQL server using command-line arguments
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+    engine = create_engine('mysql+'
+                           'mysqldb://{}:'
+                           '{}@localhost/{}'.format(sys.argv[1],
                                                                        sys.argv[2],
                                                                        sys.argv[3]),
                            pool_pre_ping=True)
